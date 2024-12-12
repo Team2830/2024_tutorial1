be05ed9ec997a7dbf67a7e44a474a8fa68965d76
Erik Orlowski <etorlows@ra.rockwell.com>
2024-12-11 21:03:24 -0600

    Tutorial

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,64 @@
+package frc.robot;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  /**
+   * This is where we create objects that our code will use.
+   */
+  TalonFX m_BottomIntake = new TalonFX(14);
+  TalonFX m_TopIntake = new TalonFX(13);
+
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    m_BottomIntake.set(1.0);
+
+  } // This is outside of the curly braces. Don't put code here!
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+}
